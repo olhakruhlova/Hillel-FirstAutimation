2fe36fdbb3a02b01fb334df4ceb199a67dd2efa5
Olha Kruhlova <olha.kruhlova@gen.tech>
2022-06-14 22:16:03 +0300

    HW №11 WebElements with annotations

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,49 +1,57 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import testdata.Email;
 
-import java.util.regex.Pattern;
-
 public class HomePage extends BasePage {
 
-    private By writeLetterButton = By.cssSelector(".primary.compose");
-    private By toInput = By.name("toFieldInput");
-    private By subjectInput = By.name("subject");
-    private By letterBody = By.id("tinymce");
-    private By sendButton = By.cssSelector(".screen__head .send.button");
-    private By bodуIFrame = By.cssSelector("#mce_0_ifr");
-
-    private By letterIsSend = By.cssSelector(".sendmsg__ads-ready");
+    @FindBy(css = ".primary.compose") //рефлексія
+    private WebElement writeLetterButtonElement;
+    @FindBy(name = "toFieldInput")
+    private WebElement toInputElement;
+    @FindBy(name = "subject")
+    private WebElement subjectInputElement;
+    @FindBy(id = "tinymce")
+    private WebElement letterBodyElement;
+    @FindBy(css = ".screen__head .send.button")
+    private WebElement sendButtonElement;
+    @FindBy(css = "#mce_0_ifr")
+    private WebElement bodуIFrameElement;
+    @FindBy(css = ".sendmsg__ads-ready")
+    private WebElement letterIsSendElement;
 
     public HomePage(WebDriver driver) {
         super(driver);
         pageUrl = "https://mail.ukr.net/desktop";
+        PageFactory.initElements(driver, this);
     }
 
     public void clickWriteLetter() {
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(writeLetterButton));
-        driver.findElement(writeLetterButton).click();
+        webDriverWait.until(ExpectedConditions.visibilityOf(writeLetterButtonElement));
+        writeLetterButtonElement.click();
     }
 
     public void writeLetter(Email email) {
-        driver.findElement(toInput).sendKeys(email.getToUser());
-        driver.findElement(subjectInput).sendKeys(email.getEmailSubject());
+        toInputElement.sendKeys(email.getToUser());
+        subjectInputElement.sendKeys(email.getEmailSubject());
         try {
-            driver.switchTo().frame(driver.findElement(bodуIFrame));
-            driver.findElement(letterBody).sendKeys(email.getEmailBody());
+            driver.switchTo().frame(bodуIFrameElement);
+            letterBodyElement.sendKeys(email.getEmailBody());
         } finally {
             driver.switchTo().parentFrame();
         }
     }
 
     public void sendLetter() {
-        driver.findElement(sendButton).click();
+        sendButtonElement.click();
     }
+
     public boolean getTextLetterIsSend(String expectedText) {
-        return webDriverWait.until(ExpectedConditions.textMatches(letterIsSend,
-                Pattern.compile("^" + expectedText + "\n.*")));
+        return webDriverWait.until(driver -> letterIsSendElement.getText()
+                .matches("^" + expectedText + "\n.*"));
     }
 }
