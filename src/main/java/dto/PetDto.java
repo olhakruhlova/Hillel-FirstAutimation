61ba8a790fb0fe40fa1f6c0762febf928c3dc8c2
Olha Kruhlova <olha.kruhlova@gen.tech>
2022-07-09 18:27:10 +0300

    LS-20_RestAssured. Create Pet

---
+++ src/main/java/dto/PetDto.java
@@ -0,0 +1,18 @@
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class PetDto {
+    private long id;
+    private String name;
+    private String status;
+
+}
